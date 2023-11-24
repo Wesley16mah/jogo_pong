e4c747de2b61bf40ab47c40eaaeddee93aa027c1
wesley.andrade <125272674+Wesley16mah@users.noreply.github.com>
2023-11-24 08:30:33 -0300

    attt

--- sketch.js
+++ sketch.js
@@ -31,9 +31,9 @@ let ponto;
 let trilha;
 
 function preload() {
-    trilha = loadSound("trilha.mp3");
-    ponto = loadSound("ponto.mp3");
-    raquetada = loadSound("raquetada.mp3");
+    trilha = loadSound("sons/trilha.mp3");
+    ponto = loadSound("sons/ponto.mp3");
+    raquetada = loadSound("sons/raquetada.mp3");
 }
 
 function setup() {
